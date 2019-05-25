1d14a1b3c23c5b26511200f96b7a7cab59541979
Megan Milstein <megan.milstein@gmail.com>
2019-05-25 02:35:40 +0000

    Updated db connection.

--- data_compiler.py
+++ data_compiler.py
@@ -2,8 +2,27 @@
 
 import pymysql
 
+db_creds = {
+        'host':'35.236.115.197', 
+        'user':'megan', 
+        'passwd':'FV5T%c*R|dNOsA4A(',
+        'database':'geoInfo',
+        'port':3306}
+
 # Open database connection
-db = pymysql.connect("localhost","megan","FV5T%c*R|dNOsA4A(","geoInfo" )
+db = pymysql.connect(
+    host=db_creds['host'],
+    user=db_creds['user'],
+    passwd=db_creds['passwd'],
+    db=db_creds['database'],
+    port=db_creds['port'],
+    ssl={'ssl':{
+            'ca':'/home/tensorflow/.keys/server-ca.pem',
+            'key':'/home/tensorflow/.keys/client-key.pem',
+            'cert':'/home/tensorflow/.keys/client-cert.pem'
+            }
+        }
+)
 
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
@@ -13,7 +32,7 @@ cursor.execute("SELECT VERSION()")
 
 # Fetch a single row using fetchone() method.
 data = cursor.fetchone()
-print ("Database version : %s " % data)
+print (data)
 
 # disconnect from server
 db.close()
